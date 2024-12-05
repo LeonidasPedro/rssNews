0d468461ff1cccd1b458770eb62d781217409bb1
leonidaspedro <leonidas@unochapeco.edu.br>
2024-12-05 11:24:26 -0300

    news api

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-
+import { fetchBreakingNews, fetchRecommendedNews } from "../../../utils/NewsApi";
+import { View, ScrollView } from "react-native";
+import NewsSection from "../../components/NewsSection/NewsSection";
+import { useQuery } from "@tanstack/react-query";
 const Home = () => {
+
+  const { data: recommendedNew, isLoading: isRecommendedLoading } = useQuery({
+    queryKey: ["recommededNewss"],
+    queryFn: fetchRecommendedNews,
+  });
+
   return (
     <>
       <Text>Home page</Text>
-        <Ionicons name="home" size={80} color="blue" />
+        <View>
+          <MiniHeader label="Recomendado" />
+          <ScrollView
+          >
+              <NewsSection
+                label="Recommendation"
+                newsProps={recommendedNew.articles}
+              />
+          </ScrollView>
+        </View>
     </>
   );
 };
---
+++ utils/ApiKey.js
@@ -0,0 +1,1 @@
+export const newsApiKey = "2af1bab8fae4471e9be5ac09b1b1e461";
